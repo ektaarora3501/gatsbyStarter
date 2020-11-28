299353c74e0bbc3ef735e339a5d2bc85bd6efd71
ektaarora3501 <ektaarora3501@gmail.com>
2020-11-28 22:08:50 +0530

    Step 1 : hello world

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      Hey Everyone!!! This is how a cool developer starts working on gatsby {":)"}
+    </div>
+  )
+}
